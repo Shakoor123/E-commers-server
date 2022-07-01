80ffd543983da340b2004162675f9e3b7a648bb7
shakoor123 <shakoorchachu2001@gmail.com>
2022-07-02 00:13:20 +0530

    finish user route using mong0($match,$project,$group)

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,30 +1,43 @@
-const jwt=require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
+const verifyToken = (req, res, next) => {
+  const authHedder = req.headers.token;
+  if (authHedder) {
+    const token = authHedder.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) {
+        res.status(401).json("token not valid");
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).json("you are not authenticated !");
+  }
+};
 
-const verifyToken=(req,res,next)=>{
-    const authHedder=req.hedders.token;
-    if (authHedder) {
-        jwt.verify(token,process.env.JWT_SEC,(err,user)=>{
-            if (err) {
-                res.status(401).json("token not valid");
-            }else{
-                req.user=user;
-                next();
-            }
-        });
-    }else{
-        return res.status(401).json("you are not authenticated !");
+const verifyTokenAndAutherization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user._id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("you are not allowed !");
     }
-}
+  });
+};
 
-const verifyTokenAndAutherization=(req,res,next)=>{
-    verifyToken(req,res,()=>{
-        if(req.user.id=== req.params.id || req.user.isAdmin){
-            next()
-        }else{
-            res.status(403).json('you are not allowed !')
-        }
-    })
-}
-
-module.exports={verifyToken,verifyTokenAndAutherization}
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("you are not allowed !");
+    }
+  });
+};
+module.exports = {
+  verifyToken,
+  verifyTokenAndAutherization,
+  verifyTokenAndAdmin,
+};
